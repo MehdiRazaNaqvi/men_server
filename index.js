1d7f1c1398e3d15f9c6688af05b492a944c03b43
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-06-16 15:46:19 +0500

    cors installed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
-
+var cors = require("cors")
 const express = require("express");
 const app = express();
 
+app.use(cors())
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }))
 
 const port = process.env.PORT || 4000
 
